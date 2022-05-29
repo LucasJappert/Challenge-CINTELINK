3fa0e48ce4225ac081d9dbf7e2ea57738377463c
Lucas Jappert <lucasnicolasjappert@gmail.com>
2022-05-29 08:27:31 -0300

    start tests

--- app/tests/dataManager.test.js
+++ app/tests/dataManager.test.js
@@ -1,11 +1,12 @@
 
-jest.mock('../services/dataManager')
+//jest.mock('../services/dataManager');
 const DataManager = require("../services/dataManager");
 describe("---------> Tests DataManager", () => {
-    // The mock factory returns the function () => false
-    DataManager.CacheNotification.mockImplementation((x) => x * 10)
-    test("Should return only sent notifications by user", () => {
-        let result = DataManager.GetSentNotificationsByUser(1);
+    jest.spyOn(DataManager, 'GetAllTags').mockReturnValue(22222);
+    test("Testing", () => {
+        //DataManager.GetAllTags.functionToMock = jest.fn().mockReturnValue(2323);
+        let result = DataManager.GetAllTags();
+        console.log(result);
         expect(1).toBe(1);
     })
 })
---
+++ app/tests/modules/notificationsProcess.test.js
@@ -0,0 +1,8 @@
+const NotificationProcess = require("../../modules/notificationsProcess");
+const NotificationUser = require("../models/NotificationUser.model");
+const DataManager = require('../services/dataManager');
+const EventEmitter = require("../helpers/eventEmitter");
+
+describe("", () => {
+
+})
